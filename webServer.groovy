f74fd1cfb33bd0577be1b3a1d432763b7a9dcc2e
Stefan Ziegler <edi.gonzales@gmail.com>
2016-02-27 20:09:36 +0100

    gitignore

--- webServer.groovy
+++ webServer.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
-@Grab(group='org.eclipse.jetty', module='jetty-server', version='9.3.7.v20160115')
-@Grab(group='org.eclipse.jetty', module='jetty-servlet', version='9.3.7.v20160115')
+@Grab(group='org.eclipse.jetty', module='jetty-server', version='8.1.7.v20120910')
+@Grab(group='org.eclipse.jetty', module='jetty-servlet', version='8.1.7.v20120910')
 
 import org.eclipse.jetty.server.Server
 import org.eclipse.jetty.servlet.*
@@ -13,9 +13,10 @@ def startJetty() {
     def handler = new ServletContextHandler(ServletContextHandler.SESSIONS)
     handler.contextPath = '/'
     handler.resourceBase = '.'
-    handler.addServlet(GroovyServlet, '/scripts/*')
-    //def filesHolder = handler.addServlet(DefaultServlet, '/')
-    //filesHolder.setInitParameter('resourceBase', './public')
+    handler.addServlet(GroovyServlet, '/scripts/*')    
+
+    def filesHolder = handler.addServlet(DefaultServlet, '/')
+    filesHolder.setInitParameter('resourceBase', './public')
 
     server.handler = handler
     server.start()
